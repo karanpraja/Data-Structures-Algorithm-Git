9da33ec85d2060e23c13e81ebff58a230e11ca80
Karan <karanpraja902@gmail.com>
2023-05-31 11:15:15 +0530

    uploading basics

---
+++ theory/basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10,b=20;
+//   cout<<"enter the value of a"<<endl;
+//   cin>>a;
+//   cout<<"enter the value of b";
+//   cin>>b;
+  cout<<"the value of a:"<<a<<endl<< "the value of b:"<<b<<endl;  
+
+}
