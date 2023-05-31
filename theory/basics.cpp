c930e7ee27ea6a9346f533aa322d15096099d5d2
Karan Prajapat <121415666+karanpraja@users.noreply.github.com>
2023-05-31 11:19:46 +0530

    Update basics.cpp

--- theory/basics.cpp
+++ theory/basics.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
 int a=10,b=20;
-//   cout<<"enter the value of a"<<endl;
-//   cin>>a;
-//   cout<<"enter the value of b";
-//   cin>>b;
+  cout<<"enter the value of a"<<endl;
+  cin>>a;
+  cout<<"enter the value of b";
+  cin>>b;
   cout<<"the value of a:"<<a<<endl<< "the value of b:"<<b<<endl;  
 
 }
